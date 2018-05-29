0805fee43933b5b373f1769bbb1175936832a808
MrPooofles <38237033+MrPooofles@users.noreply.github.com>
2018-05-29 21:50:27 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+    client.user.setActivity({game: {name: "Cesium Bot Development", type: WATCHING}});
 });
 
 client.on("guildCreate", guild => {
