f40d2d3ccfa355f7b03ab22816106516fa16f309
MrPooofles <38237033+MrPooofles@users.noreply.github.com>
2018-05-29 21:05:03 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,3 @@
-const botconfig = require("./botconfig.json");
-const Discord = require("discord.js");
-
-const bot = new Discord.Client({disableEveryone: true});
-
-bot.on("ready", async () => {
- console.log(`${bot.user.username} is online!`);
-
- bot.user.setActivity("Development of Poof Bot", {type: "WATCHING"});
-
-});
-
 // Load up the discord.js library
 const Discord = require("discord.js");
 
@@ -144,7 +132,6 @@ client.on("message", async message => {
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
-
 });
-
+           
 bot.login("NDQzNjcwODg2MzgyNTY3NDI0.DeKEvw.ttt4QFbctOmUpPvIZh6I4CGoWoE");
